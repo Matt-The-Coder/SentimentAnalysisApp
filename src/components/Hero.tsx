b046b9daa4952551e1c0fe893dfda45e82e8ec2e
Supremo1717 <ralphmatthewmanabat@gmail.com>
2024-09-15 18:14:01 +0800

    final

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,24 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Sentiment, { AnalysisResult } from 'sentiment';
 import TextField from '@mui/material/TextField';
 import ReactSpeedometer from "react-d3-speedometer"
 import data from '../assets/tagalogWords.json'
 import Box from '@mui/material/Box';
-// import InputLabel from '@mui/material/InputLabel';
-// import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 import FormControl from '@mui/material/FormControl';
-// import Select  from '@mui/material/Select';
 import { TypeAnimation } from 'react-type-animation';
-
-export const Hero = () => {
-    const [inputTagalogText, setInputTagalogText] = useState("")
-    const [inputEnglishText, setInputEnglishText] = useState("")
-    // const [language, setLanguage] = useState('')
-    // const [typeError, setTypeError] = useState(false)
+import axios from 'axios';
+interface IHero {
+    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
+}
+export const Hero:React.FC<IHero> = ({setIsLoading}) => {
+    const [selectTagalogText, setSelectTagalogText] = useState("")
+    const [selectEnglishText, setSelectEnglishText] = useState("")
+    const [postID, setPostID] = useState('')
+    const [comments, setComments] = useState([{message:""}])
+    const [errorPostID, setErrorPostID] = useState(false)
     const [result, setResult] = useState<AnalysisResult>({
         score: 0,
         comparative: 0,
@@ -23,107 +27,133 @@ export const Hero = () => {
         words: [],
         positive: [],
         negative: [],
-      });
+    });
     useEffect(() => {
-        // if (language == '') {
-        //     setTypeError(true)
-        //     setTimeout(() => {
-        //         setTypeError(false)
-        //     }, 1000)
-        // }
         const sentiment = new Sentiment()
         var tlLanguage = data
         sentiment.registerLanguage('tl', tlLanguage)
-        const englishResult = sentiment.analyze(inputEnglishText, { language: 'en' })
-        // if (language == "tl") {
-        //     setResult(tagalogResult)
-        //     console.log(tagalogResult)
-        // } else if (language == "en") { }
-            setResult(englishResult)
-        
+        const englishResult = sentiment.analyze(selectEnglishText, { language: 'en' })
+        setResult(englishResult)
 
-    }, [inputEnglishText])
+
+    }, [selectEnglishText])
 
     useEffect(() => {
-        // if (language == '') {
-        //     setTypeError(true)
-        //     setTimeout(() => {
-        //         setTypeError(false)
-        //     }, 1000)
-        // }
         const sentiment = new Sentiment()
         var tlLanguage = data
         sentiment.registerLanguage('tl', tlLanguage)
-        const tagalogResult = sentiment.analyze(inputTagalogText, { language: 'tl' })
-        // if (language == "tl") {
-        //     setResult(tagalogResult)
-        //     console.log(tagalogResult)
-        // } else if (language == "en") { }
+        const tagalogResult = sentiment.analyze(selectTagalogText, { language: 'tl' })
         console.log(tagalogResult)
-            setResult(tagalogResult)
-        
+        setResult(tagalogResult)
+    }, [selectTagalogText])
 
-    }, [inputTagalogText])
+    // const postID = '122106337976505770'
+    const pageID = '61565173125480'
+    const accessToken = 'EAALpNpGZAkzkBO1XBM0ZCNh7Oyz4vq4CmhKKPFO1HVf8fQd4mghDnA545Aka7quYcMx7etoOg4Ms5xK1fZA2edfNu4Wbmj1KZC2l1YDxxB5F5HG7XWfVZCwEZA0xI1ULbp5psJyTCyMJDetEXxMTSYIZCKNiYNztUigIxsDWUAdmvQmyGidkZBtfFO4JYBKMYMcfSR3iz0aJ'
+
+    async function getComments(e: React.FormEvent<HTMLFormElement>) {
+        try {
+            e.preventDefault()
+            if(postID.trim() !== ''){
+                setIsLoading(true)
+                const fetchedData = await axios.get(`https://graph.facebook.com/v20.0/${pageID}_${postID}/comments?access_token=${accessToken}`)
+                const result = fetchedData.data
+                console.log(result.data)
+                setComments(result.data)
+                setIsLoading(false)
+            }
+        } catch (error) {
+            setIsLoading(false)
+            console.log(error)
+            setErrorPostID(true)
+            setTimeout(()=>setErrorPostID(false), 3000)
+        }
+    }
     return (
-        <div className="hero flex sm:py-14 py-20 sm:px-20 mt-10 sm:mt-18" id='home'>
-            <div className="section1 sm:w-1/2 w-full flex flex-col gap-5 px-6 sm:py-10 justify-center">
-                <TypeAnimation
-                    sequence={[
-                        // Same substring at the start will only be typed out once, initially
-                        'Sentimental Analysis',
-                        1000, // wait 1s before replacing "Mice" with "Hamsters"
-                        'Enter your feedback',
-                        1000,
-                        'Analyze your feelings',
-                        1000
-                    ]}
-                    wrapper="span"
-                    speed={50}
-                    className='text-3xl font-semibold'
-                    style={{ fontSize: '2rem', display: 'inline-block' }}
-                    repeat={Infinity}
-                />
-                {/* {typeError && <p className='font-semibold text-xl'>Pick a Language</p>} */}
+        <div className="hero flex sm:pt-0 h-auto py-0 sm:px-20 mt-20 sm:mt-28 " id='home'>
+            <div className="section1 sm:w-1/2 w-full flex flex-col gap-2 px-6 py-6 sm:py-10 ">
+                <div className='mb-10'>
+                    <TypeAnimation
+                        sequence={[
+                            // Same substring at the start will only be typed out once, initially
+                            'Sentimental Analysis',
+                            1000, // wait 1s before replacing "Mice" with "Hamsters"
+                            'Enter your feedback',
+                            1000,
+                            'Analyze your feelings',
+                            1000
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        className='text-3xl font-semibold'
+                        style={{ display: 'inline-block' }}
+                        repeat={Infinity}
+                    />
+                    {/* {typeError && <p className='font-semibold text-xl'>Pick a Language</p>} */}
 
-                <Box sx={{ minWidth: 120 }}>
-                    {/* <FormControl fullWidth>
-                        <InputLabel id="demo-simple-select-label">Language</InputLabel>
-                        <Select
-                            required
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            onChange={(event: React.ChangeEvent<HTMLSelectElement>) => setLanguage(event.target.value)}
-                            label="Language"
-                        >
-                            <MenuItem value={"en"}>English</MenuItem>
-                            <MenuItem value={"tl"}>Tagalog</MenuItem>
-                        </Select>
-                    </FormControl> */}
+                    <form onSubmit={(e) => { getComments(e) }}>
+                        <Box sx={{ minWidth: 120, marginBottom: "0.5em" }}>
+                            <FormControl fullWidth>
+                                <TextField id="standard-basic" required value={postID} onChange={(e) => { setPostID(e.currentTarget.value) }}
+                                    label="Enter your post ID" variant="standard" />
+                                {/* {typeError && <p className='text-red-500 mt-5'>Please select a language first!</p>} */}
+                            </FormControl>
+                        </Box>
+                        {errorPostID &&                         
+                        <Box sx={{ minWidth: 120, marginBottom: "0.5em" }}>
+                        <FormControl fullWidth>
+                            <p className='error text-red-500 text-sm'>Your Post ID is invalid!</p>
+                        </FormControl>
+                        </Box>}
 
-                </Box>
-                <Box sx={{ minWidth: 120 }}>
-                    <FormControl fullWidth>
-                        <TextField id="standard-basic" onChange={(e) => { setInputEnglishText(e.currentTarget.value) }}
-                            label="Enter your English Feedback" variant="standard" />
-                        {/* {typeError && <p className='text-red-500 mt-5'>Please select a language first!</p>} */}
-
-                    </FormControl>
-
-                </Box>
-                <Box sx={{ minWidth: 120 }}>
-                    <FormControl fullWidth>
-                        <TextField id="standard-basic" onChange={(e) => { setInputTagalogText(e.currentTarget.value) }}
-                            label="Enter your Tagalog Feedback" variant="standard" />
-                        {/* {typeError && <p className='text-red-500 mt-5'>Please select a language first!</p>} */}
-
-                    </FormControl>
-
-                </Box>
+                        <Box sx={{ minWidth: 120 }}>
+                            <button className='bg-neutral-600 text-white px-4 py-2 rounded-md text-sm font-normal' type='submit'>Submit</button>
+                        </Box>
+                    </form>
+                </div>
+                {comments.length > 1 && <>
+                    <Box sx={{ minWidth: 120 }}>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Select English Comments</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                label="English Comments"
+                                value={selectEnglishText}
+                                onChange={(e) => { setSelectEnglishText(e.target.value) }}
+                            >
+                                {comments.map((e,  i)=>{
+                                       return (
+                                               <MenuItem key={i} value={e.message}>{e.message}</MenuItem> 
+                                       )
+                                })}          
+                            </Select>
+                        </FormControl>
+                    </Box>
+                    <Box sx={{ minWidth: 120 }}>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Select Tagalog Comments</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                label="Tagalog Comments"
+                                value={selectTagalogText}
+                                onChange={(e) => { setSelectTagalogText(e.target.value) }}
+                            >
+                                {comments.map((e,  i)=>{
+                                       return (
+                                               <MenuItem key={i} value={e.message}>{e.message}</MenuItem> 
+                                       )
+                                })}
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </>}
 
                 {result && (
-                    <div className='flex flex-col gap-10 justify-between py-5 sm:py-10 sm:px-10 px-5 border shadow-1xl rounded-lg'>
+                    <div className='section1 h-auto flex flex-col gap-10 justify-between py-5 sm:py-10 sm:px-10 px-5 border shadow-1xl rounded-lg'>
                         <p className='text-right sm:px-10 font-semibold text-md'>Analysis Result</p>
-                        <div className="result1 flex flex-col gap-10 pr-5 w-full">
+                        <div className="flex flex-col gap-5 w-full">
                             <p className=' font-medium'>Emotion Status                         {result.score === 0 ? <label >😐</label> : result.score < 0 && result.score >= -3 ? <label>☹️</label> :
                                 result.score < -3 ? <label>😭</label> : result.score > 0 && result.score <= 3 ? <label>😊</label> : <label>🥰</label>}</p>
                             <p className='font-medium'>Words: {result.tokens ?
@@ -137,9 +167,9 @@ export const Hero = () => {
                                     }
 
                                 }) : null}</p>
-                        </div>
 
-                        <div className="result2  flex flex-col gap-10 w-full">
+
+
                             <p className='font-medium'>Negative Words: {result.negative ?
                                 result.negative.map((e, i) => {
                                     if (i == result.negative.length - 1) {
@@ -158,10 +188,11 @@ export const Hero = () => {
                                     return (
                                         <label key={i}>{e}, </label>)
                                 }) : null}</p>
+                            <p className='font-medium'>Comparative {result.comparative}</p>
+                            <p className='font-medium'>Score: {result.score}</p>
+
+
                         </div>
-
-
-
 
                     </div>
                 )}
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,18 +1,29 @@
+import { useState } from "react"
 import { About } from "../components/About"
 import { Footer } from "../components/Footer"
 import { Header } from "../components/Header"
 import { Hero } from "../components/Hero"
+import { RiseLoader } from "react-spinners"
 
 export const Homepage = () => {
 
-
+    const [isLoading, setIsLoading] = useState(false)
     return (
         <>
-        <Header />
-        <Hero/>
-        <About/>
-        <Footer/>
-        
+            {isLoading &&
+                <>
+                    <div className="z-[55] h-lvh w-lvw fixed top-[-20px] flex justify-center items-center">
+                        <RiseLoader color="#f5f5f5" size={24} margin={5} />
+                    </div>
+                    <div className="h-lvh w-lvw fixed bg-neutral-500 top-0 z-50 bg-opacity-70">
+                    </div>
+                </>}
+
+            <Header />
+            <Hero setIsLoading={setIsLoading} />
+            <About />
+            <Footer />
+
         </>
     )
 }
